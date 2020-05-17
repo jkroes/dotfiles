791e7a85eaf76646374421d602b4763b9ae83a28
Justin Kroes <jkroes14@ucsbalum.com>
2020-05-17 13:22:41 -0700

    Swap nvim alias for path entry

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,1 +1,5 @@
+# rust
 set -g fish_user_paths ~/.cargo/bin $fish_user_paths
+
+# nvim
+set -g fish_user_paths ~/apps/nvim/usr/bin $fish_user_paths
--- .config/fish/functions/nvim.fish
+++
@@ -1,7 +0,0 @@
-# Defined in - @ line 1
-# Calling with a file argument here doesn't prevent git from using nvim as a commit editor
-# unlike callineg `e $MYVIMRC` within init.vim
-function nvim --wraps='~/apps/nvim/usr/bin/nvim' --description 'alias nvim ~/apps/nvim/usr/bin/nvim'
-  ~/apps/nvim/usr/bin/nvim $argv
-  # ~/apps/nvim/usr/bin/nvim  ~/.config/nvim/init.vim;
-end
